29f2567e1aa7e054ec50140843cb1ebf255bf3a7
Eric Berry <elberry@gmail.com>
2014-11-29 00:31:31 -0800

    Basic drop game.

---
+++ android/src/com/teammochi/games/drop/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.teammochi.games.drop.android;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.teammochi.games.drop.DropGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new DropGame(), config);
+	}
+}
