029b2e39d160e78b81616afe397b1d6b31982f43
Eric Berry <elberry@gmail.com>
2014-11-29 00:55:12 -0800

    Adding intro screen

--- core/src/com/teammochi/games/drop/DropGame.java
+++ core/src/com/teammochi/games/drop/DropGame.java
@@ -1,128 +1,27 @@
 package com.teammochi.games.drop;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
 
-import java.util.Iterator;
+public class DropGame extends Game {
 
-public class DropGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Rectangle bucket;
-	private Texture bucketImage;
-	private OrthographicCamera camera;
-	private Texture dropImage;
-	private Sound dropSound;
-	private long lastDropTime;
-	private Array<Rectangle> raindrops;
-	private Music rainMusic;
-	private Vector3 touchPos;
+	public SpriteBatch batch;
+	public BitmapFont font;
 
-	@Override
 	public void create() {
-		// load the images for the droplet and the bucket, 64x64 pixels each
-		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
-
-		// load the drop sound effect and the rain background "music"
-		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-
-		// start the playback of the background music immediately
-		rainMusic.setLooping(true);
-		rainMusic.play();
-
-		// set up the camera
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-
-		// set up sprites
 		batch = new SpriteBatch();
-
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2;
-		bucket.y = 20;
-		bucket.width = 64;
-		bucket.height = 64;
-
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
-
-		// set up everything else
-		touchPos = new Vector3();
+		//Use LibGDX's default Arial font.
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
 	}
 
-	@Override
-	public void dispose() {
-		dropImage.dispose();
-		bucketImage.dispose();
-		dropSound.dispose();
-		rainMusic.dispose();
-		batch.dispose();
-	}
-
-	@Override
 	public void render() {
-		// clear screen
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		// update camera
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-
-		// render our sprites
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for (Rectangle raindrop : raindrops) {
-			batch.draw(dropImage, raindrop.x, raindrop.y);
-		}
-		batch.end();
-
-		// handle input
-		if (Gdx.input.isTouched()) {
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			camera.unproject(touchPos);
-			bucket.x = touchPos.x - 64 / 2;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-		if (bucket.x < 0) bucket.x = 0;
-		if (bucket.x > 800 - 64) bucket.x = 800 - 64;
-
-		// update raindrops
-		if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-		Iterator<Rectangle> iter = raindrops.iterator();
-		while (iter.hasNext()) {
-			Rectangle raindrop = iter.next();
-			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if (raindrop.y + 64 < 0) {
-				iter.remove();
-			} else if (raindrop.overlaps(bucket)) {
-				dropSound.play();
-				iter.remove();
-			}
-		}
+		super.render(); //important!
 	}
 
-	private void spawnRaindrop() {
-		Rectangle raindrop = new Rectangle();
-		raindrop.x = MathUtils.random(0, 800 - 64);
-		raindrop.y = 480;
-		raindrop.width = 64;
-		raindrop.height = 64;
-		raindrops.add(raindrop);
-		lastDropTime = TimeUtils.nanoTime();
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
 }
---
+++ core/src/com/teammochi/games/drop/MainMenuScreen.java
@@ -0,0 +1,70 @@
+package com.teammochi.games.drop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+/**
+ * Created by elberry on 11/29/14.
+ */
+public class MainMenuScreen implements Screen {
+	final DropGame dropGame;
+	OrthographicCamera camera;
+
+	public MainMenuScreen(DropGame dropGame) {
+		this.dropGame = dropGame;
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		dropGame.batch.setProjectionMatrix(camera.combined);
+
+		dropGame.batch.begin();
+		dropGame.font.draw(dropGame.batch, "Welcome to Drop!!! ", 100, 150);
+		dropGame.font.draw(dropGame.batch, "Tap anywhere to begin!", 100, 100);
+		dropGame.batch.end();
+
+		if (Gdx.input.isTouched()) {
+			dropGame.setScreen(new GameScreen(dropGame));
+			dispose();
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
